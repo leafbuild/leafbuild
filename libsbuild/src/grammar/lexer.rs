a0bd79c53cd98c7ddc13e38ecf13e6d9eb28a0a9
Dinu Blanovschi <dinu.blanovschi001@gmail.com>
2020-07-09 12:24:10 +0300

    Make it compile

--- libsbuild/src/grammar/lexer.rs
+++ libsbuild/src/grammar/lexer.rs
@@ -54,19 +54,18 @@ impl<'input> Lexer<'input> {
     fn parse_number(&mut self, initial_position: usize, initial_char: char) -> Result<(usize, Tok, usize), LexicalError> {
         let result: String;
         let mut next_position: usize = initial_position + 1;
-        let type_ = if char == '0' { 0 } else { 1 };
         match self.chars.peek() {
-            None => Ok((initial_position, Tok::Number(initial_char - '0'), initial_position + 1)),
+            None => Ok((initial_position, Tok::Number((initial_char as u8 - b'0') as i32), initial_position + 1)),
             Some((i, chr)) => {
                 if initial_char == '0' {
-                    if chr == 'x' {
+                    if *chr == 'x' {
                         // parse as hex
                         self.chars.next().unwrap(); // take the 'x' out of the stream
                         let (mut num, mut end_position) = (0, initial_position + 1);
                         while let Some((pos, character)) = self.chars.next() {
                             end_position = pos;
                             if character.is_ascii_hexdigit() {
-                                num = num * 16 + hexdigit_value(character);
+                                num = num * 16 + Self::hexdigit_value(character);
                             } else {
                                 break;
                             }
@@ -78,16 +77,45 @@ impl<'input> Lexer<'input> {
                         while let Some((pos, character)) = self.chars.next() {
                             end_position = pos;
                             if character.is_digit(8) {
-                                num = num * 8 + octdigit_value(character);
+                                num = num * 8 + Self::octdigit_value(character);
                             } else {
                                 break;
                             }
                         }
                         Ok((initial_position, Tok::Number(num), end_position))
                     }
+                } else {
+                    let (mut num, mut end_position) = (0, initial_position + 1);
+                    while let Some((pos, character)) = self.chars.next() {
+                        end_position = pos;
+                        if character.is_ascii_digit() {
+                            num = num * 10 + Self::decdigit_value(character);
+                        } else {
+                            break;
+                        }
+                    }
+                    Ok((initial_position, Tok::Number(num), end_position))
                 }
             }
         }
+    }
+
+    fn octdigit_value(chr: char) -> i32 {
+        (chr as u8 - b'0') as i32
+    }
+
+    fn decdigit_value(chr: char) -> i32 {
+        (chr as u8 - b'0') as i32
+    }
+
+    fn hexdigit_value(chr: char) -> i32 {
+        let chr = chr as u8;
+        (match chr {
+            b'0'..=b'9' => chr - b'0',
+            b'a'..=b'f' => chr - b'a',
+            b'A'..=b'F' => chr - b'A',
+            _ => 0
+        }) as i32
     }
 }
 
