045fc7d2d4898920a3537736223caeb3f7c90cfc
Dinu Blanovschi <dinu.blanovschi001@gmail.com>
2020-07-15 15:43:56 +0300

    Rename grammar name

--- libcmbs/src/grammar/mod.rs
+++ libcmbs/src/grammar/mod.rs
@@ -4,10 +4,10 @@ use std::error::Error;
 mod lexer;
 /// the parser
 
-mod sbuild;
+mod cmbs;
 pub mod ast;
 
 pub fn parse(source: &str) -> Result<ast::AstProgram, Box<dyn Error>> {
-    let statements = sbuild::ProgramParser::new().parse(lexer::Lexer::new(source))?;
+    let statements = cmbs::ProgramParser::new().parse(lexer::Lexer::new(source))?;
     Ok(ast::AstProgram::new(statements))
 }
