a11c6010c57e85e11c167996a8f03166d5a107c4
Dinu Blanovschi <dinu.blanovschi001@gmail.com>
2020-07-17 00:06:18 +0300

    More operators defined

--- libcmbs/src/interpreter/ops.rs
+++ libcmbs/src/interpreter/ops.rs
@@ -1,6 +1,26 @@
 use crate::interpreter::{Value, ValueTypeMarker};
 
-pub(crate) trait OpPlus<T, Result>
+pub(crate) trait OpAdd<T, Result>
     where T: ValueTypeMarker, Result: ValueTypeMarker {
-    fn plus(val: Value<T>) -> Value<Result>;
+    fn add_to(&self, val: Value<T>) -> Value<Result>;
+}
+
+pub(crate) trait OpSubtract<T, Result>
+    where T: ValueTypeMarker, Result: ValueTypeMarker {
+    fn subtract(&self, val: Value<T>) -> Value<Result>;
+}
+
+pub(crate) trait OpMultiply<T, Result>
+    where T: ValueTypeMarker, Result: ValueTypeMarker {
+    fn multiply_with(&self, val: Value<T>) -> Value<Result>;
+}
+
+pub(crate) trait OpDivide<T, Result>
+    where T: ValueTypeMarker, Result: ValueTypeMarker {
+    fn divide_by(&self, val: Value<T>) -> Value<Result>;
+}
+
+pub(crate) trait OpModulo<T, Result>
+    where T: ValueTypeMarker, Result: ValueTypeMarker {
+    fn modulo(&self, val: Value<T>) -> Value<Result>;
 }
