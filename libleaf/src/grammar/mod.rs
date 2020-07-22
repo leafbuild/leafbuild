98502030fb9afdc98133a9d81c3312b7a86ef63b
Dinu Blanovschi <dinu.blanovschi001@gmail.com>
2020-07-23 00:48:56 +0300

    Change parser name

--- libleaf/src/grammar/mod.rs
+++ libleaf/src/grammar/mod.rs
@@ -2,10 +2,10 @@ use std::error::Error;
 
 pub mod ast;
 /// the parser
-mod cmbs;
+mod leafparser;
 mod lexer;
 
 pub fn parse(source: &str) -> Result<ast::AstProgram, Box<dyn Error>> {
-    let statements = cmbs::ProgramParser::new().parse(lexer::Lexer::new(source))?;
+    let statements = leafparser::ProgramParser::new().parse(lexer::Lexer::new(source))?;
     Ok(ast::AstProgram::new(statements))
 }
