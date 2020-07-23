633e02134b83f6ad94b1251a4b953e46b7e75653
Dinu Blanovschi <dinu.blanovschi001@gmail.com>
2020-07-23 21:43:47 +0300

    Add tools/doc.sh push

--- tools/doc.sh
+++ tools/doc.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-if [ $# -lt 2 ] || [ "$1" != "build" ] && [ "$1" != "serve" ];
+if [ $# -lt 1 ] || [ "$1" != "build" ] && [ "$1" != "serve" ] && [ "$1" != "push" ];
 then echo "$0: invoke as \"$0 <build|serve>\"" >&2
+     exit 1
 fi
 case $1 in
 "build")
@@ -13,4 +14,17 @@ case $1 in
   pushd doc || exit $?
   mdbook serve
   popd || exit $?
+  ;;
+"push")
+  pushd doc || exit $?
+  mdbook build
+  cd book || exit $?
+  git init
+  git remote add origin https://github.com/dblanovschi/leafbuild.git
+  git add -A
+  git commit -a -m "Update docs"
+  git branch gh-pages
+  git push origin gh-pages -f
+  popd || exit $?
+  ;;
 esac
